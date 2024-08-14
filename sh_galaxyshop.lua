2a2f8eca078b9f5dc36a6906c64d5432559281ea
shpaga1337jz <125084127+shpaga1337jz@users.noreply.github.com>
2024-08-14 12:06:10 +0300

    Add files via upload

---
+++ sh_galaxyshop.lua
@@ -0,0 +1,36 @@
+/*
+    BY SHPAGA
+*/
+GalaxyShop = GalaxyShop or {}
+GalaxyShop.col = {
+    ['back'] = Color(31, 33, 56, 195),
+    ['buttons'] = Color(130, 122, 173, 47),
+    ['text'] = Color(255, 255, 255)
+}
+
+GalaxyShop.Items = {
+    {
+        name = 'RPG',
+        weaponname = 'weapon_rpg',
+        price = 2000000,
+        desc = 'RPG из Half-life 2. Имеет большой урон.'
+    },
+    {
+        name = 'Ковбойка',
+        weaponname = 'swb_357_a',
+        price = 25000000,
+        desc = 'Обойма из 1337 патронов. Ваншот пушка'
+    },
+    {
+        name = 'Гаусс-пушка',
+        weaponname = 'weapon_gauss',
+        price = 1250000,
+        desc = 'Гаусс пушка - лучшая вещь для передвижения!'
+    },
+    {
+        name = 'SMG [HL2]',
+        weaponname = 'weapon_smg',
+        price = 1000000,
+        desc = 'Пистолет-пулемёт с возможностью стрелять гранатами.'
+    },
+}
---
+++ sv.lua
@@ -0,0 +1,31 @@
+/*
+    BY SHPAGA
+*/
+GalaxyShop = GalaxyShop or {}
+util.AddNetworkString( 'GalaxyShop_recode')
+
+net.Receive( 'GalaxyShop_recode', function( len, ply ) 
+    local id = net.ReadInt(32)
+    local itemData = GalaxyShop.Items[id]
+    if not itemData then return end
+
+    local canAfford = ply:canAfford(itemData.price)
+    local HasWeapon = ply:HasWeapon(itemData.weaponname)
+    if HasWeapon then
+        ply:ChatPrint( 'Покупка не прошла. Вы уже имеете это оружие' )
+        return 
+    end
+
+    if not canAfford then
+        ply:ChatPrint( 'У вас не хватает средств!' )
+        return 
+    end
+
+    ply:addMoney(-itemData.price)
+    ply:Give( itemData.weaponname )
+
+    if itemData.weaponname == 'weapon_smg' then
+        ply:GiveAmmo(15, 'SMG1_Grenade', false)
+    end
+
+end)
